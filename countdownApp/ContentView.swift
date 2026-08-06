756c66cedf75c5618ddd756033c135c1d1e98d8e
ArrayOfLilly <arrayoflilly@gmail.com>
2026-08-06 22:26:26 +0200

    Initial Commit

---
+++ countdownApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  countdownApp
+//
+//  Created by Ildikó Kasza on 2026. 08. 06..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ countdownApp/countdownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdownAppApp.swift
+//  countdownApp
+//
+//  Created by Ildikó Kasza on 2026. 08. 06..
+//
+
+import SwiftUI
+
+@main
+struct countdownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
